62e532addfe0b411f89d54fef6a1b108433b7e7d
anonze <lwisiano@gmail.com>
2023-06-25 01:48:12 +0300

    (bug fix)Fixed a bug: NextAuth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import ToasterContext from './context/ToasterContext';
+import AuthContext from './context/AuthContext';
 
 export const metadata = {
     title: 'Messenger Clone',
@@ -14,8 +15,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                <ToasterContext />
-                {children}
+                <AuthContext>
+                    <ToasterContext />
+                    {children}
+                </AuthContext>
             </body>
         </html>
     );
